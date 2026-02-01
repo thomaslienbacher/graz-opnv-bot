fffb8bd3771dcdf7c5a138cf2e04acc6366e18a0
Thomas Lienbacher <lienbacher.tom@gmail.com>
2026-02-01 22:21:13 +0100

    Added new database entries and cargo update

--- execute-bot.sh
+++ execute-bot.sh
@@ -2,6 +2,6 @@
 
 echo "Running graz-opnv-bot at $(date)"
 
-# if the messae is empty nothing will be sent
+# if the message is empty nothing will be sent
 /home/thomas/.cargo/bin/matrix-commander-rs -m "$(target/release/graz-opnv-bot)" 2>&1
 
